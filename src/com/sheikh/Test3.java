56f7e0795745e482c4c77295071f2d6d90ecd87d
Sheikh Qumruzzaman <squmruzz@cisco.com>
2018-11-27 16:52:15 -0800

    init

--- src/com/sheikh/Test3.java
+++ src/com/sheikh/Test3.java
@@ -1,12 +1,63 @@
 package com.sheikh;
 
+import java.util.HashSet;
+
 public class Test3 {
     public Test3() {
         //System.out.println(climbStairs(0, 3));
         //System.out.println(climbStairsTab(4));
         int[] arr = new int[]{2,7,9,3,1};
         //System.out.println(houseRobber(arr, arr.length - 1));
-        System.out.println(houseRobberTab(arr));
+        //System.out.println(houseRobberTab(arr));
+        int[] nums = {2, 2, 3, 1, 3};
+        //System.out.println(subarraySum(nums, 7));
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        System.out.println(isValidSudoku(board));
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] col = new HashSet[9];
+        HashSet<Character>[][] box = new HashSet[3][3];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                // check row
+                if (row[i] == null) row[i] = new HashSet<>();
+                if (row[i].contains(board[i][j])) {
+                    return false;
+                }
+                else row[i].add(board[i][j]);
+                // check column
+                if (col[j] == null) col[j] = new HashSet<>();
+                if (col[j].contains(board[i][j])) {
+                    return false;
+                }
+                else col[j].add(board[i][j]);
+                // check box
+                int k = i / 3;
+                int l = j / 3;
+                if (box[k][l] == null) box[k][l] = new HashSet<>();
+                if (box[k][l].contains(board[i][j])) {
+                    return false;
+                }
+                else box[k][l].add(board[i][j]);
+            }
+        }
+
+        return true;
     }
 
     public int climbStairs(int j, int n) {
